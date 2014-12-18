a8a09240737ea0f6ef4bbec2a681e50c0a8aa746
Rhenan Medeiros <rhenan.lm@gmail.com>
2014-12-18 18:37:57 -0200

    removendo modernizer do nosso cabeçalho e algumas funções que não estavam sendo usadas no nosso mais.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,8 @@
     $(function() {
         $('a[rel~=external]').attr('target', '_blank');
 
-        $('.selectcustom').customSelect();
-        
-        //Abrindo Menu
-        $('.navbar-twitch-toggle').on('click', function(event) {
-            event.preventDefault();
-            $('.navbar-twitch').toggleClass('open');
-        });
-        
-//        $(".treetable").treetable({ 
-//            expandable: true,
-//            indent: 25,
-//            expanderTemplate: "<a href='#' class='expander awesome-tooltip' data-toggle='tooltip' data-placement='top' title=''></a>",
-//            stringExpand: "Vizualizar filhos",
-//            stringCollapse: "Vizualizar filhos"
-//        });
-        
-        $('.awesome-tooltip').tooltip();
-        
-        $(":file").filestyle();
-        
-        
-        $('.scroll-pane').jScrollPane();
-        
+
         
     });
-    
-    
-     $.fn.innerstaticHeight = function() {
-        var heightOfOuterfixed = $('.navbar-twitch.edc').height(),
-            offset = $('.content-menu-lateral-edc').offset(),
-            topOfInnerstatic2 = offset.top,
-            potentialHeight = heightOfOuterfixed - topOfInnerstatic2;
 
-        $('.content-menu-lateral-edc').css('max-height', potentialHeight);
-    };
-    
 })(jQuery, window, document); 
